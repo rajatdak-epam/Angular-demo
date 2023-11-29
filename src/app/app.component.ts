a00d2e0ad7acfc6cfbb235bfae93a90018a6e888
Rajat Dak <rajat_dak@epam.com>
2023-11-29 17:30:40 +0530

    Lifecycle method implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,62 @@
-import { Component } from '@angular/core';
+import {
+  Component, OnInit, AfterContentChecked, AfterContentInit, AfterViewChecked,
+  AfterViewInit, DoCheck, OnChanges, OnDestroy, ViewChild, ContentChild
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ChildComponent } from './child/child.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ChildComponent, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Angular-demo';
+export class AppComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked, OnDestroy {
+
+  displayChild: boolean = false;
+  @ViewChild(ChildComponent, {static:false}) child?: ChildComponent;
+  @ContentChild(ChildComponent) contentChild?: ChildComponent;
+
+  constructor() {
+    console.log("AppComponent:Constructor");
+  }
+
+  toggle() {
+    this.displayChild = !this.displayChild;
+  }
+
+  ngOnChanges() {
+    console.log("AppComponent:OnChanges");
+  }
+
+  ngOnInit() {
+    console.log("AppComponent:OnInit");
+  }
+
+  ngDoCheck() {
+    console.log("AppComponent:DoCheck");
+  }
+  ngAfterContentInit() {
+    console.log("AppComponent:AfterContentInit");
+  }
+
+  ngAfterContentChecked() {
+    console.log("AppComponent:AfterContentChecked", this.contentChild);
+  }
+
+  ngAfterViewInit() {
+    console.log("AppComponent:AfterViewInit");
+  }
+
+  ngAfterViewChecked() {
+    console.log("AppComponent:AfterViewChecked", this.child);
+  }
+
+  ngOnDestroy() {
+    console.log("AppComponent:OnDestroy");
+  }
 }
