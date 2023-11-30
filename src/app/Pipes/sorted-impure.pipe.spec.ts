77baf29a3db277a1217089dce16b2f0ab950b27a
Rajat Dak <rajat_dak@epam.com>
2023-11-30 10:46:59 +0530

    Added Pipes example

---
+++ src/app/Pipes/sorted-impure.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SortedImpurePipe } from './sorted-impure.pipe';
+
+describe('SortedImpurePipe', () => {
+  it('create an instance', () => {
+    const pipe = new SortedImpurePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
