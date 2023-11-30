0a04f75247943d80d5032dbfeb17708a3ffaf818
Rajat Dak <rajat_dak@epam.com>
2023-11-30 09:49:20 +0530

    added change detection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LifecycleComponent } from './lifecycle/lifecycle.component';
+import { ChangeDetectorComponent } from './change-detector/change-detector.component';
 
 export const routes: Routes = [
-    //{ path: '', redirectTo: '/lifecycle', pathMatch: 'full' },
+    { path: '', redirectTo: '/lifecycle', pathMatch: 'full' },
     {path: 'lifecycle', component: LifecycleComponent},
+    { path: 'cd', component: ChangeDetectorComponent },
 ];
